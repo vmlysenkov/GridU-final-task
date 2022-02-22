1bdac763518f8196ce6171cac6e90c977f3074fe
Vasilii Lysenkov <vlysenkov@griddynamics.com>
2022-02-22 12:19:09 +0300

    added logic for counting days and hours before course completion

--- src/main/java/CourseCompletion.java
+++ src/main/java/CourseCompletion.java
@@ -1,15 +1,24 @@
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class CourseCompletion {
     public static int startWorkingHour = 10;
     public static int endWorkingHour = 18;
 
     public static ArrayList<Integer> getTimeFromCourseCompletion(Calendar endDate, int coursesDuration) {
-        ArrayList<Integer> daysAndHoursAfterCourseCompletion = new ArrayList<>();
-        int daysFromCourseCompletion = 0;
-        int hoursFromCourseCompletion = 0;
-        endDate.set(Calendar.HOUR_OF_DAY, 24);
+        ArrayList<Integer> daysAndHoursBeforeOrAfterCourseCompletion = new ArrayList<>();
+        int daysAfterCourseCompletion = 0;
+        int daysBeforeCourseCompletion = 0;
+        int hoursAfterCourseCompletion = 0;
+        int hoursBeforeCourseCompletion = 0;
+        int amountOfWorkingHoursAtLastDay = coursesDuration % DetermineCourseCompletion.workHoursPerDay;
+        endDate.set(Calendar.HOUR_OF_DAY, 23);
+        endDate.set(Calendar.MINUTE, 59);
         Calendar currentDate = Calendar.getInstance();
         int hoursToday = currentDate.get(Calendar.HOUR_OF_DAY);
         System.out.println(currentDate.get(Calendar.HOUR_OF_DAY));
@@ -19,23 +28,54 @@ public class CourseCompletion {
         if (currentDate.getTimeInMillis() > endDate.getTimeInMillis()) {
             do {
                 currentDate.add(Calendar.DAY_OF_MONTH, -1);
-                daysFromCourseCompletion++;
+                daysAfterCourseCompletion++;
             } while (currentDate.getTimeInMillis() > endDate.getTimeInMillis());
 
             if (hoursToday >= startWorkingHour && hoursToday <= endWorkingHour) {
                 if (Calendar.getInstance().get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY && Calendar.getInstance().get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
-                    hoursFromCourseCompletion = hoursToday - startWorkingHour;
-                    daysFromCourseCompletion--;
+                    hoursAfterCourseCompletion = hoursToday - startWorkingHour;
+                    daysAfterCourseCompletion--;
                 }
             } else {
-                hoursFromCourseCompletion = 0;
+                hoursAfterCourseCompletion = 0;
             }
             if ((coursesDuration % DetermineCourseCompletion.workHoursPerDay) != 0) {
-                hoursFromCourseCompletion = hoursFromCourseCompletion + endWorkingHour - (coursesDuration % DetermineCourseCompletion.workHoursPerDay + startWorkingHour);
+                hoursAfterCourseCompletion = hoursAfterCourseCompletion + endWorkingHour - (coursesDuration % DetermineCourseCompletion.workHoursPerDay + startWorkingHour);
             }
-            daysAndHoursAfterCourseCompletion.add(daysFromCourseCompletion);
-            daysAndHoursAfterCourseCompletion.add(hoursFromCourseCompletion);
+            daysAndHoursBeforeOrAfterCourseCompletion.add(daysAfterCourseCompletion - 1);
+            daysAndHoursBeforeOrAfterCourseCompletion.add(hoursAfterCourseCompletion);
+        } else {
+            Predicate<Calendar> isWeekend = date -> date.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY
+                    || date.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY;
+            LocalDate today = LocalDate.ofInstant(currentDate.toInstant(), ZoneId.systemDefault());
+            LocalDate finish = LocalDate.ofInstant(endDate.toInstant(), ZoneId.systemDefault());
+            List<LocalDate> workingDays = today.datesUntil(finish.plusDays(1))
+                    .collect(Collectors.toList());
+            if (amountOfWorkingHoursAtLastDay == 0) {
+                if (hoursToday < startWorkingHour) {
+                    daysBeforeCourseCompletion = workingDays.size();
+                } else if (hoursToday >= startWorkingHour && hoursToday <= endWorkingHour) {
+                    daysBeforeCourseCompletion = workingDays.size() - 1;
+                    hoursBeforeCourseCompletion = endWorkingHour - hoursToday;
+                } else {
+                    daysBeforeCourseCompletion = workingDays.size() - 1;
+                }
+            } else {
+                if (hoursToday < startWorkingHour) {
+                    daysBeforeCourseCompletion = workingDays.size() - 1;
+                    hoursBeforeCourseCompletion = amountOfWorkingHoursAtLastDay;
+                } else if (hoursToday >= startWorkingHour && hoursToday <= endWorkingHour) {
+                    daysBeforeCourseCompletion = workingDays.size() - 2;
+                    hoursBeforeCourseCompletion = endWorkingHour - hoursToday + amountOfWorkingHoursAtLastDay;
+                } else {
+                    daysBeforeCourseCompletion = workingDays.size() - 2;
+                    hoursBeforeCourseCompletion = amountOfWorkingHoursAtLastDay;
+                }
+            }
+            daysAndHoursBeforeOrAfterCourseCompletion.add(daysBeforeCourseCompletion);
+            daysAndHoursBeforeOrAfterCourseCompletion.add(hoursBeforeCourseCompletion);
+            System.out.println(workingDays);
         }
-        return daysAndHoursAfterCourseCompletion;
+        return daysAndHoursBeforeOrAfterCourseCompletion;
     }
 }
--- src/test/java/DetermineCourseCompletionTest.java
+++ src/test/java/DetermineCourseCompletionTest.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class DetermineCourseCompletionTest {
     @Test
     public void dateTest1() {
-        Calendar startDate = new GregorianCalendar(2022, 0, 27);
+        Calendar startDate = new GregorianCalendar(2022, Calendar.FEBRUARY, 4);
         int coursesDuration = 64;
 
         Calendar actual = DetermineCourseCompletion.getCourseCompletionDate(startDate, coursesDuration);
-        Calendar expected = new GregorianCalendar(2022, 1, 7);
+        Calendar expected = new GregorianCalendar(2022, Calendar.FEBRUARY, 15);
         expected.set(Calendar.HOUR_OF_DAY, 18);
 
         assertEquals(expected.getTime(), actual.getTime());
@@ -21,7 +21,7 @@ public class DetermineCourseCompletionTest {
 
     @Test
     public void dateTest2() {
-        Calendar startDate = new GregorianCalendar(2022, 1, 18);
+        Calendar startDate = new GregorianCalendar(2022, Calendar.FEBRUARY, 18);
         int coursesDuration = 30;
 
         Calendar actual = DetermineCourseCompletion.getCourseCompletionDate(startDate, coursesDuration);
@@ -41,7 +41,7 @@ public class DetermineCourseCompletionTest {
         ArrayList<Integer> expected = new ArrayList<>();
         expected.add(4);
         expected.add(0);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -54,6 +54,32 @@ public class DetermineCourseCompletionTest {
         ArrayList<Integer> expected = new ArrayList<>();
         expected.add(3);
         expected.add(2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateDaysAndHoursBeforeCourseCompletion1() {
+        Calendar startDate = new GregorianCalendar(2022, Calendar.FEBRUARY, 16);
+        int coursesDuration = 64;
+
+        Calendar endDate = DetermineCourseCompletion.getCourseCompletionDate(startDate, coursesDuration);
+        ArrayList<Integer> actual = CourseCompletion.getTimeFromCourseCompletion(endDate, coursesDuration);
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(3);
+        expected.add(6);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateDaysAndHoursBeforeCourseCompletion2() {
+        Calendar startDate = new GregorianCalendar(2022, Calendar.FEBRUARY, 21);
+        int coursesDuration = 30;
+
+        Calendar endDate = DetermineCourseCompletion.getCourseCompletionDate(startDate, coursesDuration);
+        ArrayList<Integer> actual = CourseCompletion.getTimeFromCourseCompletion(endDate, coursesDuration);
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(1);
+        expected.add(12);
         assertEquals(expected, actual);
     }
 }
