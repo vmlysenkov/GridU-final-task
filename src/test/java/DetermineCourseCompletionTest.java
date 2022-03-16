df07b8705b93004f801cd48a50cf278592786545
Vasilii Lysenkov <vlysenkov@griddynamics.com>
2022-03-16 15:14:50 +0300

    created new test class

---
+++ src/test/java/DetermineCourseCompletionTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import static org.junit.Assert.*;
+
+public class DetermineCourseCompletionTest {
+
+    @Test
+    public void shouldDetermineEndDateWhenCourseEndsAt6PM() {
+        LocalDateTime startDate = LocalDateTime.of(2022, Month.FEBRUARY, 4, 10, 0);
+        int coursesDuration = 64;
+
+        LocalDateTime actual = DetermineCourseCompletion.getCourseCompletionDate(startDate, coursesDuration);
+        LocalDateTime expected = LocalDateTime.of(2022, Month.FEBRUARY, 15, 18, 0);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDetermineEndDateWhenCourseEndsDuringWorkingDay() {
+        LocalDateTime startDate = LocalDateTime.of(2022, Month.FEBRUARY, 18, 10, 0);
+        int coursesDuration = 30;
+
+        LocalDateTime actual = DetermineCourseCompletion.getCourseCompletionDate(startDate, coursesDuration);
+        LocalDateTime expected = LocalDateTime.of(2022, Month.FEBRUARY, 23, 16, 0);
+        assertEquals(expected, actual);
+    }
+}
