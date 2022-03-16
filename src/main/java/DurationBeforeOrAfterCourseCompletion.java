95cc4edc090fff52b4c7b7d5178f8229b199f374
Vasilii Lysenkov <vlysenkov@griddynamics.com>
2022-03-16 10:03:15 +0300

    Lombok library connected

--- src/main/java/DurationBeforeOrAfterCourseCompletion.java
+++ src/main/java/DurationBeforeOrAfterCourseCompletion.java
@@ -1,5 +1,6 @@
-import java.util.Objects;
+import lombok.Value;
 
+@Value
 public class DurationBeforeOrAfterCourseCompletion {
     int days;
     int hours;
@@ -7,26 +8,5 @@ public class DurationBeforeOrAfterCourseCompletion {
     public DurationBeforeOrAfterCourseCompletion(int days, int hours) {
         this.days = days;
         this.hours = hours;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        DurationBeforeOrAfterCourseCompletion that = (DurationBeforeOrAfterCourseCompletion) o;
-        return days == that.days && hours == that.hours;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(days, hours);
-    }
-
-    @Override
-    public String toString() {
-        return "DurationBeforeOrAfterCourseCompletion{" +
-                "days=" + days +
-                ", hours=" + hours +
-                '}';
     }
 }
